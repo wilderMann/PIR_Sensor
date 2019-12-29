c7e43f223053c1f7c31a600f517828d4dbeb68c7
wilderMann <andreas.wildmann@gmail.com>
2019-12-29 22:31:35 +0100

    deleted unnecessary code

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,6 @@ uint8_t flag_heartBeat = 0;
 uint8_t isOn = 0;
 unsigned long lastReconnectAttempt = 0;
 string ClientID;
-Ticker heartBeat;
 
 void setup() {
         Serial.begin(115200);
@@ -64,7 +63,6 @@ void setup() {
         ClientID = string(CLIENTID) + DEVICE_NAME;
         httpServer_ini();
         attachInterrupt(digitalPinToInterrupt(PIN), isrHandler, RISING);
-        heartBeat.attach(55.0, heartBeatHandler);
 }
 
 
@@ -90,16 +88,6 @@ void loop(){
                 string topic = "homie/" + string(DEVICE_NAME) + "/pir-sensor/motion";
                 client.publish(topic.c_str(),payload.c_str(),true);
                 flag_motion = 0;
-        }
-        if(flag_heartBeat) {
-                long time = millis() / 1000;
-                string topic = "homie/" + string(DEVICE_NAME) + "/$stats/uptime";
-                char payload[20];
-                sprintf(payload, "%ld", time);
-                client.publish(topic.c_str(), payload,true);
-                topic = "homie/" + string(DEVICE_NAME) + "/$stats/interval";
-                client.publish(topic.c_str(), "60",true);
-                flag_heartBeat = 0;
         }
         if(isOn){
           int val = 1;
@@ -156,10 +144,6 @@ void handleStatus() {
 
 void isrHandler(){
         flag_motion = 1;
-}
-
-void heartBeatHandler(){
-        flag_heartBeat = 1;
 }
 
 void callback(char *topic, byte *payload, unsigned int length){
